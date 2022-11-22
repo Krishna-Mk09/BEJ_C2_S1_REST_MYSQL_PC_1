a79d8157ad9743e2d9f6a1be673dd9da0bfb91e5
M V K <krishna.mugala9@gmail.com>
2022-11-22 11:51:43 +0530

    added logics to the methods in the impl class

--- src/test/java/com/niit/dp/BEJ_C2_S1_REST_MYSQL_PC_1/service/ProductServiceImpl.java
+++ src/test/java/com/niit/dp/BEJ_C2_S1_REST_MYSQL_PC_1/service/ProductServiceImpl.java
@@ -10,39 +10,44 @@ package com.niit.dp.BEJ_C2_S1_REST_MYSQL_PC_1.service;
 
 import com.niit.dp.BEJ_C2_S1_REST_MYSQL_PC_1.domain.Product;
 import com.niit.dp.BEJ_C2_S1_REST_MYSQL_PC_1.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ProductServiceImpl implements ProductService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
+    @Autowired
     public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
     public Product saveProduct(Product product) {
-        return null;
+        return productRepository.save(product);
     }
 
     @Override
     public boolean deleteProduct(int productId) {
-        return false;
+        productRepository.deleteById(productId);
+        return true;
     }
 
     @Override
     public List<Product> fetchAllProducts() {
-        return null;
+        return productRepository.findAll();
     }
 
     @Override
     public Optional<Product> fetchProductById(int productId) {
-        return Optional.empty();
+        return productRepository.findById(productId);
     }
 
     @Override
     public void updateProduct(int productId, Product product) {
-
+        productRepository.save(product);
     }
 }
