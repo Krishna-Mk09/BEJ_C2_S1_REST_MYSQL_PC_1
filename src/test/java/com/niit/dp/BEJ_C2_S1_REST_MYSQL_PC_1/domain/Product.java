a925cabd993e50439bbc934617e61def43168508
M V K <krishna.mugala9@gmail.com>
2022-11-22 11:26:06 +0530

    created product class in domain package

---
+++ src/test/java/com/niit/dp/BEJ_C2_S1_REST_MYSQL_PC_1/domain/Product.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name : M.Krishna.
+ * Date: 22-11-2022
+ * Created With: IntelliJ IDEA Community Edition
+ *
+ */
+
+
+package com.niit.dp.BEJ_C2_S1_REST_MYSQL_PC_1.domain;
+
+public class Product {
+
+}
