daa6afa34fb70f6453ecf834d17e6b16ad97beab
M V K <krishna.mugala9@gmail.com>
2022-11-22 11:57:45 +0530

    created the product controller class in controller package

--- src/test/java/com/niit/dp/BEJ_C2_S1_REST_MYSQL_PC_1/service/ProductServiceImpl.java
+++ src/test/java/com/niit/dp/BEJ_C2_S1_REST_MYSQL_PC_1/service/ProductServiceImpl.java
@@ -79,7 +79,6 @@ public class ProductServiceImpl implements ProductService {
      */
     @Override
     public void updateProduct(int productId, Product product) {
-
         productRepository.save(product);
     }
 }
