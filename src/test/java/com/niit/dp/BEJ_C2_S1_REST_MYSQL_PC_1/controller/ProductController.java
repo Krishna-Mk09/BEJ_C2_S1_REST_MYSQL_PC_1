a1b7b93e8499c7448a7c8780f0900a4304c8a424
M V K <krishna.mugala9@gmail.com>
2022-11-22 12:01:37 +0530

    called the service interface in the product controller

--- src/test/java/com/niit/dp/BEJ_C2_S1_REST_MYSQL_PC_1/controller/ProductController.java
+++ src/test/java/com/niit/dp/BEJ_C2_S1_REST_MYSQL_PC_1/controller/ProductController.java
@@ -8,5 +8,16 @@
 
 package com.niit.dp.BEJ_C2_S1_REST_MYSQL_PC_1.controller;
 
+import com.niit.dp.BEJ_C2_S1_REST_MYSQL_PC_1.service.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
 public class ProductController {
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 }
